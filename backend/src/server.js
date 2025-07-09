5053160bb0ab0814cf118cc8a7405b994da9c340
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-07-09 16:17:03 -0300

    Adição da pasta public com todos os arquivos html,css e js junto com a criação do servidor fastifystatic para hospedagem do projeot

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,10 @@ const Fastify = require('fastify');
 const { PrismaClient } = require('@prisma/client');
 const authRoutes = require('./routes/auth');
 
+const path = require('path');
+const fastifyStatic = require('@fastify/static');
+
+
 const app = Fastify({ logger: true });
 const prisma = new PrismaClient();
 
@@ -11,6 +15,11 @@ const presencaRoutes = require('./routes/presencas');
 const produtoRoutes = require('./routes/produto');
 const carrinhoRoutes = require('./routes/carrinho');
 
+// Depois de criar o app Fastify:
+app.register(fastifyStatic, {
+  root: path.join(__dirname, '..', '..', 'public'),
+  prefix: '/',
+});
 app.decorate('prisma', prisma);
 app.register(authRoutes, { prefix: '/auth' });
 app.register(jogoRoutes);
