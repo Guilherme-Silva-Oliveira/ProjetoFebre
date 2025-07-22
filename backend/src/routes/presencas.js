83840707f689ea9ae2622209f0a9b819900220a0
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-07-22 18:27:54 -0300

    criação das funções confirmar e retirar presença

--- backend/src/routes/presencas.js
+++ backend/src/routes/presencas.js
@@ -2,6 +2,8 @@ const presencaController = require('../controllers/presencaController');
 
 async function presencaRoutes(fastify, options) {
   fastify.post('/presenca', presencaController.confirmarPresenca);
+  fastify.post('/presenca/confirmar', presencaController.confirmarPresenca);
+  fastify.post('/presenca/retirar', presencaController.retirarPresenca);
 }
 
 module.exports = presencaRoutes;
